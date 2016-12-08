bd9ea150285f0ea3d67d60143c27568591816adf
frog <austindanielfrench@gmail.com>
2016-12-07 18:16:06 -0800

    Assignment complete

--- LessonTwo/LessonTwo/HailstormPart1.cs
+++ LessonTwo/LessonTwo/HailstormPart1.cs
@@ -4,41 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LessonTwo
-{
-    class HailstormPart1
+            namespace LessonTwo
     {
-
-        public static int getPosition(int position)
+        class HailstormPart1
         {
-            int newPosition = 0;
 
-            /*
-             * Today's assignment is very simple: calculate a new position for a hailstone in a storm.
-             * If the argument "position" is even, set newPosition to it divided by two
-             * If "position" is odd, set newPosition to it times 3 plus 1
-             * ---
-             * If you are stumped and need a hint, scroll down to the bottom, but only use it if you need to
+            public static int getPosition(int position)
+            {
+                int newPosition = 0;
+
+                /*
+                 * Today's assignment is very simple: calculate a new position for a hailstone in a storm.
+                 * If the argument "position" is even, set newPosition to it divided by two
+                 * If "position" is odd, set newPosition to it times 3 plus 1
+                 * ---
+                 * If you are stumped and need a hint, scroll down to the bottom, but only use it if you need to
+                 * */
+
+                //START CODING HERE
+                if (newPosition % 2 == 1)
+                {
+                    newPosition = position * 3 + 1; 
+                }
+
+                else
+                {
+                    newPosition = position / 2;
+                }
+                //STOP CODING HERE
+
+                // Ignore this, we'll get into it next lesson
+                return newPosition;
+            }
+
+            // HINT
+
+            /* 
+             * You can test if a number is even or odd using modulus division.
+             * For "x % 2":
+             * if x is odd, it will return 1
+             * if x is even it will return 0
              * */
 
-            //START CODING HERE
-
-
-
-            //STOP CODING HERE
-
-            // Ignore this, we'll get into it next lesson
-            return newPosition;
         }
-
-        // HINT
-
-        /* 
-         * You can test if a number is even or odd using modulus division.
-         * For "x % 2":
-         * if x is odd, it will return 1
-         * if x is even it will return 0
-         * */
-
     }
-}
+    
